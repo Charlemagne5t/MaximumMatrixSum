0ab82ed7a4626e76a63370d703e2f923672a025c
alek <wait82985@gmail.com>
2026-01-05 17:26:58 +0500

    probelm solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,21 +1,22 @@
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
-        long res = 0L;
-        int c = 0;
-        int min = Integer.MAX_VALUE;
-        for (int[] r : matrix) {
-            for (int v : r) {
-                if (v < 0) {
-                    c++;
-                    min = Math.min(min, -v);
-                    res += -v;
-                } else {
-                    min = Math.min(min, v);
-                    res += v;
+        long res = 0;
+        int neg = 0;
+        int min = Math.abs(matrix[0][0]);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] < 0) {
+                    neg++;
                 }
+                res += Math.abs(matrix[i][j]);
+
+
+                min = Math.min(Math.abs(matrix[i][j]), min);
             }
         }
-
-        return c % 2 == 0 ? res : res - min * 2;
+        if (neg % 2 == 0) {
+            return res;
+        }
+        return res - min * 2;
     }
 }
